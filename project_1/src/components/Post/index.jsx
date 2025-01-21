4137693606297b7b1eb9d846bd1c7a8125ce1f6c
fat83dotcom <fernando.quimic@gmail.com>
2025-01-21 12:33:17 -0300

    juste de codigo

--- project_1/src/components/Post/index.jsx
+++ project_1/src/components/Post/index.jsx
@@ -1,18 +1,14 @@
 import { PostCard } from "../PostcCard"
+import './style.css'
 
 export const Post = (props) => {
-    const posts = props
-    console.log(posts);
-    
-    return (
-        <div className='posts'>
-          {posts.map(post => (
-            <PostCard 
-              key={post.id}
-              title={post.title}
-              body={post.body}
-            />
-          ))}
-        </div>
-    )
+  <div className='posts'>
+    {props.map(post => (
+      <PostCard 
+        key={post.id}
+        title={post.title}
+        body={post.body}
+      />
+    ))}
+  </div>
 }
