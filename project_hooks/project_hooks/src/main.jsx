2584a126be918de86e1d3d62e9f4e1324246fa4c
fat83dotcom <fernando.quimic@gmail.com>
2025-01-23 10:44:33 -0300

    Tela inicial,

--- project_hooks/project_hooks/src/main.jsx
+++ project_hooks/project_hooks/src/main.jsx
@@ -1,13 +1,29 @@
-import { StrictMode } from 'react'
+import { StrictMode, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import RegisterCustomer from './Pages/RegisterCustomers/ResgisterCustomer'
 import RegisterProduct from './Pages/RegisterProduct/RegisterProduct'
+import { HeaderNavBar } from './Components/HeaderNavBar'
 
+export const Home = () => {
+    const [homeComponent, setHomeComponent] = useState(<h1>Escolha uma opção!</h1>)
+
+    const handleHeaderClick = (setComponent) => {
+        {setComponent === 'customer' && setHomeComponent(<RegisterCustomer/>)}
+        {setComponent === 'product' && setHomeComponent(<RegisterProduct/>)}
+        {setComponent === 'order' && setHomeComponent(<h2>Página de vendas</h2>)}
+    }
+
+    return (
+        <>
+            <HeaderNavBar click={handleHeaderClick}/>
+            {homeComponent}
+        </>
+    )
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RegisterCustomer/>
-    <RegisterProduct/>
+    <Home/>
   </StrictMode>,
 )
