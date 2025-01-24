2c9c3a9bbcf032f1b0958db95059c5aaa7881eb8
fat83dotcom <fernando.quimic@gmail.com>
2025-01-24 16:48:38 -0300

    Adicionando os botoes para as novas paginas.paginas

--- project_hooks/project_hooks/src/Components/HeaderNavBar/index.jsx
+++ project_hooks/project_hooks/src/Components/HeaderNavBar/index.jsx
@@ -8,6 +8,9 @@ export const HeaderNavBar = (click) => {
                 <span onClick={() => click.click('customer')}>Cadastrar Cliente</span>
                 <span onClick={() => click.click('product')}>Cadastar Produto</span>
                 <span onClick={() => click.click('order')}>Efetuar Venda</span>
+                <span onClick={() => click.click('searchCustomers')}>Consultar Clientes</span>
+                <span onClick={() => click.click('searchProducts')}>Consultar Produtos</span>
+                <span onClick={() => click.click('SearchOrders')}>Consultar Vendas</span>
             </div>
         </nav>
         </>
