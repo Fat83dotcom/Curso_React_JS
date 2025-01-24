25cb569043e3d51b09f8fb01c286e9019fda2e07
fat83dotcom <fernando.quimic@gmail.com>
2025-01-24 16:54:23 -0300

    Aplicando os useMemo.

--- project_hooks/project_hooks/src/Components/ProductForm/index.jsx
+++ project_hooks/project_hooks/src/Components/ProductForm/index.jsx
@@ -1,5 +1,5 @@
 import './style.css'
-import { useState } from 'react'
+import { useState, useMemo, useCallback } from 'react'
 import { ProductButton } from '../ProductButton'
 import { ClearButton } from '../ClearButton'
 import { handleSubmitData } from '../../Utils/ApiCalls'
@@ -16,28 +16,7 @@ export const ProductForm = () => {
     )
     // const [buttonStatus, setButtonStatus] = useState(false)
 
-    const handleChange = (event) => {
-        const { name, value } = event.target;
-        setFormData(
-            (prevData) => ({...prevData, [name]: value,})
-        )
-    }
-
-    const handleRegisterClick = async () => {
-        if (formData.name && formData.price && formData.quantity) {
-            const war = await handleSubmitData("http://127.0.0.1:8000/register_product/", 'POST', formData, handleClearForm)
-            handleWarning(war)
-        }else {
-            handleWarning('Faltam Dados no Formulario')
-        }
-    }
-
-    const handleWarning = async (msg) => {
-        setRegisterStatus(msg)
-        await new Promise(() => setTimeout(() => {setRegisterStatus('')}, 3000))
-    }
-
-    const handleClearForm = () => {
+    const handleClearForm = useCallback(() => {
         setFormData(
             {
                 name: '',
@@ -45,10 +24,32 @@ export const ProductForm = () => {
                 quantity: '',
             }
         )
+    }, [setFormData])
+
+    const handleWarning = useCallback(async (msg) => {
+        setRegisterStatus(msg)
+        await new Promise(() => setTimeout(() => {setRegisterStatus('')}, 3000))
+    }, [])
+
+    const handleRegisterClick = useCallback(async () => {
+        if (formData.name && formData.price && formData.quantity) {
+            const war = await handleSubmitData("http://127.0.0.1:8000/register_product/", 'POST', formData, handleClearForm)
+            handleWarning(war)
+        }else {
+            handleWarning('Faltam Dados no Formulario')
+        }
+    }, [formData, handleWarning, handleClearForm])
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormData(
+            (prevData) => ({...prevData, [name]: value,})
+        )
     }
 
     return (
         <>
+            <Warning warning={registerStatus}/>
             <div className="product-form">
                 <label htmlFor="name">Nome Produto</label>
                 <input value={formData.name}
@@ -60,9 +61,8 @@ export const ProductForm = () => {
                 <input value={formData.quantity}
                 onChange={handleChange} type="number" name="quantity" id="quantity" />
             </div>
-            <ClearButton click={handleClearForm}/>
-            <ProductButton click={handleRegisterClick}/>
-            <Warning warning={registerStatus}/>
+            {useMemo(() => <ClearButton click={handleClearForm}/>, [handleClearForm])}
+            {useMemo(() => <ProductButton click={handleRegisterClick}/>, [handleRegisterClick])}
         </>
     )
 }
