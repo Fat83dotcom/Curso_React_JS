e0b090239851ce95f468e1407e4713fbcd7007ed
fat83dotcom <fernando.quimic@gmail.com>
2025-01-27 17:29:52 -0300

    Criando o formulario de vendas.

--- project_hooks/project_hooks/src/Components/OrderForm/index.jsx
+++ project_hooks/project_hooks/src/Components/OrderForm/index.jsx
@@ -1,10 +1,15 @@
 import { useCallback, useState } from 'react'
 import './style.css'
 import { handleSubmitGet } from '../../Utils/ApiCalls'
+import { Warning } from '../Warning'
 
 
 export const OrderForm = () => {
+    const [warning, setWarning] = useState('')
     const [searchCustomer, setSearchCustomer] = useState('')
+    const [customerSearched, setCustomerSearched] = useState([])
+    const [customerId, setCustomerId] = useState(0)
+
 
     const handleClearForm = () => {
         setSearchCustomer('')
@@ -12,25 +17,60 @@ export const OrderForm = () => {
 
     const handleSearchClick = async () => {
         const url = `http://127.0.0.1:8000/search_customer/?search_name=${searchCustomer}`
+        const data = await handleSubmitGet(url)
 
-        const data = await handleSubmitGet(url,handleClearForm)
-        console.log(data);
+        if (data.msg === 'Sucesso.'){
+            handleClearForm()
+        }
+        handleWarning(data.msg)
+        console.log(data.data);
+
+        setCustomerSearched(data.data)
+    }
+
+    const handleIdClick = () => {
+        
+    }
+
+    const handleOrderCustomerId = (e) => {
+        const id = e.target.text
+        setCustomerId(id)
+        console.log(id);
 
     }
 
-    const handleOnChangeCustomerSearch = useCallback((e) => {
+    const handleWarning = useCallback(async (msg) => {
+        setWarning(msg)
+        await new Promise(() => setTimeout(() => {setWarning('')}, 3000))
+    }, [setWarning])
+
+    const handleOnChangeCustomerSearch = (e) => {
         const customer = e.target.value
         setSearchCustomer(customer)
-    }, [setSearchCustomer])
+    }
 
     return (
-        <div className='conteiner'>
-            <div className='form-container'>
-                <label htmlFor="customer">Buscar por Cliente</label>
-                <input onChange={handleOnChangeCustomerSearch} type="search" name="customer" id="customer" />
-                <p>{searchCustomer}</p>
-                <button onClick={handleSearchClick}>Buscar</button>
+        <>
+            <div className='conteiner'>
+                <div className='form-container'>
+                    <label htmlFor="customer">Buscar por Cliente</label>
+                    <input onChange={handleOnChangeCustomerSearch} value={searchCustomer} type="search" name="customer" id="customer" />
+                    <button onClick={handleSearchClick}>Buscar</button>
+                </div>
+                <div className='found-container'>
+                    <h2>Clientes Encontrados:</h2>
+                    {customerSearched.map((customer) => {
+                        return (
+                            <div className='customer-container' key={customer.id}>
+                                <a onClick={handleOrderCustomerId}>{customer.id}</a>
+                                <p>{customer.name}</p>
+                                <p>{customer.second_name}</p>
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
-        </div>
+            <Warning warning={warning}/>
+        </>
     )
 }
