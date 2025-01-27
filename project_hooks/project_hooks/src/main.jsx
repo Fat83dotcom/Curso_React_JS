ace94b600e0a936e9ba23ba21dc9c0e43a5b02cd
fat83dotcom <fernando.quimic@gmail.com>
2025-01-27 11:41:14 -0300

    Importando o novo componente.

--- project_hooks/project_hooks/src/main.jsx
+++ project_hooks/project_hooks/src/main.jsx
@@ -7,6 +7,7 @@ import { HeaderNavBar } from './Components/HeaderNavBar'
 import { SearchCustomerPage } from './Pages/SearchCustmers'
 import { SearchOrdersPage } from './Pages/SearchOrders'
 import { SearchProducts } from './Pages/SearchProducts'
+import { RegisterOrder } from './Pages/OrderPage/RegisterOrder'
 
 export const Home = () => {
     const [homeComponent, setHomeComponent] = useState(<h1>Escolha uma opção!</h1>)
@@ -14,7 +15,7 @@ export const Home = () => {
     const handleHeaderClick = (setComponent) => {
         {setComponent === 'customer' && setHomeComponent(<RegisterCustomer/>)}
         {setComponent === 'product' && setHomeComponent(<RegisterProduct/>)}
-        {setComponent === 'order' && setHomeComponent(<h2>Página de vendas</h2>)}
+        {setComponent === 'order' && setHomeComponent(<RegisterOrder/>)}
         {setComponent === 'searchCustomers' && setHomeComponent(<SearchCustomerPage/>)}
         {setComponent === 'searchProducts' && setHomeComponent(<SearchProducts/>)}
         {setComponent === 'SearchOrders' && setHomeComponent(<SearchOrdersPage/>)}
