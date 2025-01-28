d410de6a844695a9f8439c2d52dc679bfdcb83df
fat83dotcom <fernando.quimic@gmail.com>
2025-01-28 15:43:09 -0300

    Modificando o componente para alternar entre eles.

--- project_hooks/project_hooks/src/Pages/OrderPage/RegisterOrder.jsx
+++ project_hooks/project_hooks/src/Pages/OrderPage/RegisterOrder.jsx
@@ -1,12 +1,20 @@
+import { useState } from 'react'
+import { Order } from '../../Components/Order'
 import { OrderForm } from '../../Components/OrderForm'
 import './style.css'
 
 
 export const RegisterOrder = () => {
+    const [changePage, setChangePage] = useState('orderForm')
+    const [customerId, setCustomerId] = useState(0)
     return (
         <>
             <h1>Vendas</h1>
-            <OrderForm/>
+            {changePage === 'orderForm' && <OrderForm change={(change) => setChangePage(change)} customId={(id)=> setCustomerId(id)}/>}
+            {changePage === 'order' && <Order customerId={customerId} change={(change) => setChangePage(change)}/>}
+
         </>
     )
 }
+
+<Order/>
