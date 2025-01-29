63d1825c3e7bf857c752f3240f325c2ee729f2e2
fat83dotcom <fernando.quimic@gmail.com>
2025-01-29 18:00:57 -0300

    Implementando o display das vendas.

--- project_hooks/project_hooks/src/Components/Order/index.jsx
+++ project_hooks/project_hooks/src/Components/Order/index.jsx
@@ -1,49 +1,91 @@
-import { useEffect, useState, useCallback } from 'react'
+import { useState, useCallback } from 'react'
 import './style.css'
 import P from 'prop-types'
-import { handleSubmitPost } from '../../Utils/ApiCalls'
+import { handleSubmitGet, handleSubmitPost } from '../../Utils/ApiCalls'
 import { Warning } from '../Warning'
 
-
-export const Order = ({customerId, change}) => {
-
-    const [warning, setWarning] = useState('teste')
-
-    const handleChangePage = useCallback(() => {
-        change('orderForm')
-    }, [change])
-
-    const handleWarning = useCallback(async (msg) => {
-        setWarning(msg)
-        await new Promise((resolve) => setTimeout(() => {setWarning(''); resolve()}, 3000))
-    }, [setWarning])
-
-    const handleFetchOrder = useCallback(async() => {
-        const body = {id_customer: customerId, total: 0}
-        const data = await handleSubmitPost(
-            'http://127.0.0.1:8000/create_order/',
-            body
-        )
-        handleWarning(data.msg)
-        console.log(data);
-    }, [handleWarning, customerId])
-
-    useEffect(() => {
-        handleFetchOrder()
-    }, [])
-
+const OrderDisplay = ({handleFetchOrder, orderData, orderSearchData, handleChangePage}) => {
     return (
         <>
-            <Warning warning={warning}/>
+            <button onClick={handleFetchOrder}>Criar Pedido</button>
             <div className='container-order-main'>
                 <div className='container-order'>
                     <h2>Pedido</h2>
                     <div>
                         <div>
                             <h3>Dados do Pedido</h3>
+                            {orderData.map((data) => {
+                                return (
+                                    <div className='order-customer-open' key={data.pk}>
+                                        <div>
+                                        <p>Código Pedido</p>
+                                        <a>{data.pk}</a>
+                                        </div>
+                                        <div>
+                                        <p>Data</p>
+                                        <p>{data.date}</p>
+                                        </div>
+                                        <div>
+                                        <p>Valor Total</p>
+                                        <p>{data.total}</p>
+                                        </div>
+                                        <div>
+                                        <p>Status Pedido</p>
+                                        <p>{data.order_status}</p>
+                                        </div>
+                                    </div>
+                                )
+                            })}
                         </div>
                         <div>
                             <h3>Dados do Cliente</h3>
+                            {orderData.map((data) => {
+                                return (
+                                    <div className='order-customer-open' key={data.pk}>
+                                        <div>
+                                        <p>Nome</p>
+                                        <p>{data.customer_name}</p>
+                                        </div>
+                                        <div>
+                                        <p>Sobrenome</p>
+                                        <p>{data.customer_second_name}</p>
+                                        </div>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                        <div>
+                            <h3>Pedidos do Cliente em Aberto</h3>
+                            {orderSearchData.map((data) => {
+                                return (
+                                    <div className='order-customer-open' key={data.pk}>
+                                        <div>
+                                        <p>Nome</p>
+                                        <p>{data.customer_name}</p>
+                                        </div>
+                                        <div>
+                                        <p>Sobrenome</p>
+                                        <p>{data.customer_second_name}</p>
+                                        </div>
+                                        <div>
+                                        <p>Código Pedido</p>
+                                        <a>{data.pk}</a>
+                                        </div>
+                                        <div>
+                                        <p>Data</p>
+                                        <p>{data.date}</p>
+                                        </div>
+                                        <div>
+                                        <p>Valor Total</p>
+                                        <p>{data.total}</p>
+                                        </div>
+                                        <div>
+                                        <p>Status Pedido</p>
+                                        <p>{data.order_status}</p>
+                                        </div>
+                                    </div>
+                                )
+                            })}
                         </div>
                     </div>
                     <div>
@@ -54,13 +96,76 @@ export const Order = ({customerId, change}) => {
                     <h3>Lista de itens</h3>
                 </div>
             </div>
-            <p>{customerId}</p>
             <button onClick={handleChangePage}>Voltar à página anterior.</button>
         </>
     )
 }
 
+export const Order = ({customerId, change}) => {
+
+    const [warning, setWarning] = useState('')
+    const [orderCode, setOrderCode] = useState('')
+    const [orderData, setOrderData] = useState([])
+    const [orderSearchData, setOrderSearchData] = useState([])
+
+    const handleChangePage = useCallback(() => {
+        change('orderForm')
+    }, [change])
+
+    const handleWarning = useCallback(async (msg) => {
+        setWarning(msg)
+        await new Promise((resolve) => setTimeout(() => {setWarning(''); resolve()}, 3000))
+    }, [setWarning])
+
+    const handleFetchSearchOrder = useCallback(async () => {
+        const url = `http://127.0.0.1:8000/search_order/?search_order=${customerId}`
+        const data = await handleSubmitGet(url)
+        if (data.data) {
+            return data.data
+        }
+        return {}
+    }, [customerId])
+
+    const handleFetchOrder = useCallback(async () => {
+        const body = {id_customer: customerId, total: 0}
+        const data = await handleSubmitPost(
+            'http://127.0.0.1:8000/create_order/',
+            body
+        )
+
+        if (data.data) {
+            const orderData = await handleFetchSearchOrder()
+            setOrderData([orderData])
+            handleWarning(data.msg)
+        } else {
+            const orderDataUnauthorized = await handleFetchSearchOrder()
+            setOrderSearchData([orderDataUnauthorized])
+            handleWarning(data.msg)
+        }
+    }, [handleWarning, setOrderData, customerId, handleFetchSearchOrder])
+
+    return (
+        <>
+            <Warning warning={warning}/>
+            <OrderDisplay
+                handleFetchOrder={handleFetchOrder}
+                orderData={orderCode}
+                orderSearchData={orderSearchData}
+                handleChangePage={handleChangePage}
+            />
+
+        </>
+    )
+}
+
 Order.propTypes = {
-    customerId: P.number,
+    customerId: P.string,
     change: P.func,
+}
+
+OrderDisplay.propTypes = {
+    handleFetchOrder: P.func,
+    orderData: P.array,
+    orderSearchData: P.array,
+    handleChangePage: P.func
 }
