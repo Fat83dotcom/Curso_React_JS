6c3290be7a30907456d1b87d0f2e2f52fa0b3e15
fat83dotcom <fernando.quimic@gmail.com>
2025-02-03 17:50:55 -0300

    Mudando para arrow function

--- project_hooks/project_hooks/src/Components/Order/OrderComponents/OrderAppendItems/index.jsx
+++ project_hooks/project_hooks/src/Components/Order/OrderComponents/OrderAppendItems/index.jsx
@@ -9,7 +9,7 @@ const initialState = {
     lastFuncProductCall: ''
 }
 
-function reducer(state, action) {
+const reducer = (state, action) => {
     switch (action.type) {
         case "SET_LAST_FUNC":
             return {...state, lastFuncProductCall: action.payload}
