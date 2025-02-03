65e8371d2612957294ed8e9ed8a0ceb6caa1c55a
fat83dotcom <fernando.quimic@gmail.com>
2025-02-02 21:35:01 -0300

    Criando uma chamada delete para a api

--- project_hooks/project_hooks/src/Utils/ApiCalls.js
+++ project_hooks/project_hooks/src/Utils/ApiCalls.js
@@ -41,3 +41,26 @@ export const handleSubmitGet = async (url) => {
         console.error( `Verifique sua conexão com a internet.${error}`)
     }
 }
+
+export const handleSubmitDelete = async (url) => {
+    try {
+        const response = await fetch(url, {
+            method: 'DELETE',
+        })
+        console.log(await response.text());
+
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data, response.status);
+            return {data, response: response.status}
+
+        } else {
+            const data = await response.json();
+            console.error("Erro ao deletar o item:", {response: response.status});
+            return  {data, response: response.status}
+        }
+    } catch (error) {
+        console.error("Erro na requisição:", error)
+        return  {msg: `Erro na requisição: ${error}`}
+    }
+}
