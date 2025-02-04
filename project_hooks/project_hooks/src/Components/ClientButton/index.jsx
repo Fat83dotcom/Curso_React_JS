40b67e405ecfd9ee32aaf95206ed7cc1a820cf04
fat83dotcom <fernando.quimic@gmail.com>
2025-02-04 16:49:44 -0300

    incluindo proptypes

--- project_hooks/project_hooks/src/Components/ClientButton/index.jsx
+++ project_hooks/project_hooks/src/Components/ClientButton/index.jsx
@@ -1,7 +1,12 @@
 import './style.css'
+import P from 'prop-types'
 
-export const ClientSubmitButton = (click) => {
+export const ClientSubmitButton = ({click}) => {
     return (
-        <button className='style-button' onClick={click.click}>Cadastrar Cliente</button>
+        <button className='style-button' onClick={click}>Cadastrar Cliente</button>
     )
+}
+
+ClientSubmitButton.propTypes = {
+    click: P.func
 }
