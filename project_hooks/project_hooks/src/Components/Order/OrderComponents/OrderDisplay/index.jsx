aaaa8869d3c88ba26e16cbe8b8477b2090d06c6f
fat83dotcom <fernando.quimic@gmail.com>
2025-02-04 10:44:19 -0300

    Implementando o novo display para os pedidos do cliente.

--- project_hooks/project_hooks/src/Components/Order/OrderComponents/OrderDisplay/index.jsx
+++ project_hooks/project_hooks/src/Components/Order/OrderComponents/OrderDisplay/index.jsx
@@ -1,6 +1,6 @@
 import P from 'prop-types'
 
-export const OrderDisplay = ({orderData, orderSearchData, handleCloseOrder}) => {
+export const OrderDisplay = ({orderData, orderAllData, handleCloseOrder}) => {
     return (
         <>
             <div className='container-order'>
@@ -52,15 +52,12 @@ export const OrderDisplay = ({orderData, orderSearchData, handleCloseOrder}) => 
                                 })}
                             </tbody>
                         </table>
-
                     </div>
                     <div className='center-tables'>
                     <h3>Pedidos do Cliente</h3>
                         <table>
                             <thead>
                                 <tr>
-                                    <th>Nome</th>
-                                    <th>Sobrenome</th>
                                     <th>Código Pedido</th>
                                     <th>Data</th>
                                     <th>Valor Total</th>
@@ -68,12 +65,10 @@ export const OrderDisplay = ({orderData, orderSearchData, handleCloseOrder}) => 
                                 </tr>
                             </thead>
                             <tbody>
-                                {orderSearchData ? orderSearchData.map((data) => {
+                                {orderAllData ? orderAllData.map((data) => {
                                     return (
                                         <tr key={data.pk}>
-                                            <td>{data.customer_name}</td>
-                                            <td>{data.customer_second_name}</td>
-                                            <td><a>{data.pk}</a></td>
+                                            <td><a>{data.id}</a></td>
                                             <td>{data.date}</td>
                                             <td>{data.total.toFixed(2)}</td>
                                             <td>{data.order_status ? 'Aberto' : 'Fechado'}</td>
@@ -92,7 +87,7 @@ export const OrderDisplay = ({orderData, orderSearchData, handleCloseOrder}) => 
 OrderDisplay.propTypes = {
     handleFetchOrder: P.func,
     orderData: P.array,
-    orderSearchData: P.array,
     handleChangePage: P.func,
-    handleCloseOrder: P.func
+    handleCloseOrder: P.func,
+    orderAllData: P.array
 }
