61f5df2a4651b686394de57bd7e78d43739932c0
fat83dotcom <fernando.quimic@gmail.com>
2025-02-04 16:56:32 -0300

    Montando a logica das msg com o redux.

--- project_hooks/project_hooks/src/Components/Warning/index.jsx
+++ project_hooks/project_hooks/src/Components/Warning/index.jsx
@@ -1,7 +1,33 @@
-export const Warning = (warning) => {
+import { useDispatch, useSelector } from "react-redux"
+import { changeWarning } from "../../features/warning/warningSlice"
+import { useEffect } from "react"
+import P from "prop-types"
+
+export const Warning = () => {
+    const warning = useSelector((state) => state.warning.value)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        // dispatch(changeWarning(msg()))
+        const timer =  () => {
+            new Promise((resolve) => {
+                setTimeout(() => {
+                    dispatch(changeWarning('---'))
+                    resolve()
+                    console.log('debug');
+                }, 3000)
+            })
+        }
+        timer()
+    })
+
     return (
-        <div>
-            <h4>{warning.warning}</h4>
+        <div className="style-warning">
+            <h4> {warning} </h4>
         </div>
     )
+}
+
+Warning.propTypes = {
+    msg: P.func
 }
