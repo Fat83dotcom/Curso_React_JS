42651af47b043839bbfc4062bd3de0e1b34d7ee1
fat83dotcom <fernando.quimic@gmail.com>
2025-02-05 14:57:28 -0300

    ajustando os cabeçalhos de importação

--- project_hooks/project_hooks/src/main.jsx
+++ project_hooks/project_hooks/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode, useReducer, useState } from 'react'
+import { StrictMode, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import RegisterCustomer from './Pages/RegisterCustomers/ResgisterCustomer'
