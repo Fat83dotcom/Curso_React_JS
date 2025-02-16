52947c9baff564d80c4757d8a531b96a15a726c4
fat83dotcom <fernando.quimic@gmail.com>
2025-02-16 20:44:10 -0300

    refactor(ProductButton): substituir botão customizado por componente Button do Mantine
    - Substituiu o botão customizado (`<button>`) pelo componente `Button` do Mantine, utilizando a variante `submitButton`.
    - Removida a importação do arquivo CSS (`style.css`), já que os estilos agora são gerenciados pelo Mantine.
    - Nenhuma mudança funcional foi realizada; trata-se de uma refatoração para melhorar a consistência visual e a integração com o tema do Mantine.

--- project_hooks/project_hooks/src/Components/ProductButton/index.jsx
+++ project_hooks/project_hooks/src/Components/ProductButton/index.jsx
@@ -1,7 +1,10 @@
-import './style.css'
+// import './style.css'
+
+import { Button } from '@mantine/core'
 
 export const ProductButton = (click) => {
     return (
-        <button className='style-button' onClick={click.click}>Cadastrar Produto</button>
+        // <button className='style-button' onClick={click.click}>Cadastrar Produto</button>
+        <Button variant='submitButton' onClick={click.click}>Cadastrar Produto</Button>
     )
 }
